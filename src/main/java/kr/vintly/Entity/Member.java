5eb056bfd060eff154bcdccc49dd6b879a82bc17
kimyezzang97 <kimyezzang97@gmail.com>
2024-04-02 22:57:14 +0900

    Member Entity class 생성

---
+++ src/main/java/kr/vintly/Entity/Member.java
@@ -0,0 +1,82 @@
+package kr.vintly.Entity;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+import org.springframework.data.annotation.CreatedDate;
+
+import java.sql.Date;
+import java.sql.Timestamp;
+import java.time.LocalDateTime;
+
+
+@Entity(name = "member")
+@Getter
+@ToString()
+//@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Member {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int idx;
+
+    @Column(name = "member_id")
+    private String memberId;
+
+    private String pw;
+
+    private String name;
+
+    private String address;
+
+    @Column(name = "detail_address")
+    private String detailAddress;
+
+    private String nickname;
+
+    private Date birth;
+
+    private String email;
+
+    private String gender;
+
+    @Column(name = "use_yn")
+    private String useYn;
+
+    @Column(name = "reg_date")
+    @CreatedDate
+    private Timestamp regDate;
+
+    @Column(name = "pw_date")
+    @CreatedDate
+    private Timestamp pwDate;
+
+    @Column(name = "del_date")
+    private Timestamp delDate;
+
+    @Column(name = "email_code")
+    private String emailCode;
+
+    @Column(name = "email_ex_date")
+    @CreatedDate()
+    private Timestamp emailExDate;
+
+    protected Member(String memberId, String pw, String name, String address,
+                     String detailAddress, String nickname, Date birth, String email,
+                     String gender, String emailCode){
+        this.memberId = memberId;
+        this.pw = pw;
+        this.name = name;
+        this.address = address;
+        this.detailAddress = detailAddress;
+        this.nickname = nickname;
+        this.birth = birth;
+        this.email = email;
+        this.gender = gender;
+        this.useYn = "K"; // 이메일 인증 대기
+        this.emailCode = emailCode;
+        this.emailExDate = Timestamp.valueOf(LocalDateTime.now().plusDays(3)); //만든 날짜 보다 3일 후 입력
+    }
+}
